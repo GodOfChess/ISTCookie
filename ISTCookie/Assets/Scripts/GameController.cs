f49c171d053c760fd04b610a619f3f7f7240d38e
GodOfChess <71175092+GodOfChess@users.noreply.github.com>
2021-02-10 00:09:38 +0500

    Загрузка проекта ISTCookie на GitHub
    Создан проект в Unity 2019
    Настроена камера для игры
    Добавлены элементы UI для стартового меню
    Создан скрипт для гейм-контроллера
    Добавлены шрифты

---
+++ ISTCookie/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] int balance;
+    public Text balancetext;
+    
+    public void ButtonClick()
+    {
+        balance += 1;
+    }
+    public void Update()
+    {
+        balancetext.text = balance.ToString();
+    }
+}
