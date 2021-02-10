6065bc09bae14365cb14843a1477eb594829f90a
GodOfChess <71175092+GodOfChess@users.noreply.github.com>
2021-02-10 23:54:25 +0500

    Добавление новой сцены
    Добавлена новая сцена "Достижения"
    Добавлен задний фон
    Добавлена кнопка перехода с главной сцены на сцену "Достижения"
    Добавлены PlayerPrefs

---
+++ ISTCookie/Assets/Scripts/AchieveScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AchieveScript : MonoBehaviour
+{
+    private int allbalance;
+    public Text welcomeText;
+
+    public void Start()
+    {
+        allbalance = PlayerPrefs.GetInt("allbalance");
+        welcomeText.text = "Всего очков : " + allbalance.ToString();
+    }
+}
--- ISTCookie/Assets/Scripts/GameController.cs
+++ ISTCookie/Assets/Scripts/GameController.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] int balance;
+    [SerializeField] int allbalance;
     public Text balancetext;
     
+    public void Start()
+    {
+        balance = PlayerPrefs.GetInt("balance");
+        allbalance = PlayerPrefs.GetInt("allbalance");
+    }
+
     public void ButtonClick()
     {
         balance += 1;
+        allbalance += 1;
+        PlayerPrefs.SetInt("balance", balance);
+        PlayerPrefs.SetInt("allbalance", allbalance);
     }
+
+    public void ToAchievements()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void Update()
     {
         balancetext.text = balance.ToString();
